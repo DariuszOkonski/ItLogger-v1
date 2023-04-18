874e935dff38e358d54e16eb2fe41145d8e6924c
Dariusz Okoński <darek200180@gmail.com>
2023-04-18 19:03:18 +0200

    add material ui and clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    // init materialize js
+    M.AutoInit();
+  });
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1>Hello world</h1>
-      </header>
+      <h1>Hello world</h1>
     </div>
   );
 }
